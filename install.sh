c3b76e868de0a2c2e96be4668066219234a99a46
Nilson Ferreira de Souza Jr <nfsouzaj@MacBook-Pro.localdomain>
2023-08-16 00:20:48 -0400

    feat: v2

--- install.sh
+++ install.sh
@@ -1,35 +1,85 @@
-#!/bin/bash
-# set -o errexit   # abort on nonzero exitstatus
-# set -o nounset   # abort on unbound variable
-# set -o pipefail  # don't hide errors within pipes
+# #!/bin/bash
+# # set -o errexit   # abort on nonzero exitstatus
+# # set -o nounset   # abort on unbound variable
+# # set -o pipefail  # don't hide errors within pipes
 
-set -uo pipefail
+# set -uo pipefail
 
 export SERVERS=3
 export AGENTS=2
 export CILIUM_VERSION=1.14.0
+export MEM_SERVER=4G
+export MEM_AGENT=8G
 
+#### -z string
+####  True if the length of string is zero.
+
+
+if [ -z "$1" ];
+then
+    echo ""
+    echo "#################################################################################################"
+    echo "looks like you didn't write anything when calling this script so lemme try creating a cluster..."
+    echo "#################################################################################################"
+    echo ""
+    echo "#######################################"
+    echo "checking cluster:"
+    k3d cluster list k3s
+    echo "#######################################"
+    echo ""
+    if [[ $? -eq 0 ]]
+    then
+        echo ""
+        sl
+        cowsay "ops....moooo"
+        echo "\033[31m";
+        echo "ops..."
+        echo "looks like a cluster already exist, you need to pass delete when calling the script in order to recreate it..."
+        echo "exiting script"
+        echo "\033[00m";
+        echo ""
+        exit 1
+    fi
+else
+    if [ "$1" = "delete" ]; 
+    then
+        k3d cluster list k3s
+        if [[ $? -eq 0 ]];
+        then
+            echo "Deleting exiting cluster k3s..."
+            k3d cluster delete k3s
+            sleep 3
+        else
+            echo "k3s cluster doesn't exist... creating one, now!"
+        fi
+    fi
+fi
+
+echo ""
 echo "#######################################################"
-echo "############### k3d with k3s and cilium ###############"
+echo "########### Deploying k3d with k3s and cilium #########"
 echo "############### creating cluster with: ################"
-echo "############### servers: ${SERVERS} ############################"
-echo "############### agents: ${AGENTS} #############################"
+echo "############### servers: ${SERVERS} ${MEM_SERVER} ##########################"
+echo "############### agents: ${AGENTS} ${MEM_AGENT} ###########################"
 echo "#######################################################"
+echo ""
 sleep 5
 
 k3d cluster create k3s \
 --servers ${SERVERS} \
---servers-memory 6G \
+--servers-memory ${MEM_SERVER} \
 --agents ${AGENTS} \
---agents-memory 8G \
+--agents-memory ${MEM_AGENT} \
 --k3s-arg="--disable=traefik@server:*" \
 --k3s-arg="--disable-network-policy@server:*" \
 --k3s-arg="--flannel-backend=none@server:*" \
 --k3s-arg=feature-gates="NamespaceDefaultLabelName=true@server:*"
 
+echo ""
 echo "#######################################################"
 echo "########## starting  docker exec mounts... ############"
 echo "#######################################################"
+echo ""
 
 sleep 5
 counts=0
@@ -40,6 +90,8 @@ counta=0
 echo "#######################################################"
 echo "################ mounting servers #####################"
 echo "#######################################################"
+echo ""
+
 while [ $counts -lt ${SERVERS} ]
 do
     echo "working on docker exec for server:" $counts
@@ -48,9 +100,12 @@ do
     sleep 3
 done
 
+echo ""
 echo "#######################################################"
 echo "################# mounting agents #####################"
 echo "#######################################################"
+echo ""
+
 while [ $counta -lt ${AGENTS} ]
 do
     echo "working on docker exec for agent:" $counta
@@ -59,15 +114,20 @@ do
     sleep 3
 done
 
+echo ""
 echo "############################################################"
 echo "#### tainting nodes to allow pods to land on ready only ####"
 echo "############################################################"
+echo ""
 sleep 5
 kubectl taint node -l beta.kubernetes.io/instance-type=k3s node.cilium.io/agent-not-ready=true:NoSchedule --overwrite=true
 sleep 5
+
+echo ""
 echo "#######################################################"
 echo "############# deploying cilium via helm ###############"
 echo "#######################################################"
+echo ""
 sleep 5
 helm repo add cilium https://helm.cilium.io/
 
@@ -80,44 +140,74 @@ helm install cilium cilium/cilium --version=${CILIUM_VERSION} \
     --set hubble.ui.enabled=true \
     --set global.kubeProxyReplacement="strict" --namespace kube-system
 
-until bash -c "kubectl get pods -n kube-system -l k8s-app=cilium | grep 'Init:CreateContainerError'"; do \
-    kubectl get pods -n kube-system -l k8s-app=cilium | grep "Running"; \
-    is_running=$${?}; \
-    if test $${is_running} -eq 0; then \
-        echo "time to jump into the next task:" \
-        break; \
-    else \
+echo ""
+echo "#######################################################"
+echo "############# waiting pods to be ok.... ###############"
+echo "#######################################################"
+echo ""
+
+until bash -c "kubectl get pods -n kube-system -l k8s-app=cilium | grep -q 'Init:CreateContainerError'" 
+do
+    kubectl get pods -n kube-system -l k8s-app=cilium | grep "Running"
+    if [[ ${?} -eq 0 ]]
+    then
+        echo "time to jump into the next task:"
+        break
+    else
         echo "waiting for pods to be in the right state..."
-        sleep 5; \
-    fi; \
+        sleep 5
+    fi
 done
 
 counts=0
 counta=0
 
+echo ""
 echo "##############################################################"
 echo "######## starting  docker exec mounts for servers... #########"
 echo "##############################################################"
+echo ""
+sleep 3
 
 while [ $counts -lt ${SERVERS} ]
 do 
     echo "working on docker exec for node:" $counts
-    docker exec k3d-k3s-server-$counts sh -c "mount --make-shared /run/cilium/cgroupv2"
+    # If you want an empty string and nonnumeric values to be evaluated as zero (0), use the following syntax.
+    while [[ $(docker exec k3d-k3s-server-"${counts}" sh -c "mount --make-shared /run/cilium/cgroupv2") -eq 1 ]]
+    do
+        echo "Houston, we have a problem. Result is no good:" $?
+        echo "trying mount again"
+        sleep 5
+    done
     ((counts++))
     sleep 3
-done 
+done
 
+echo ""
 echo "##############################################################"
 echo "######## starting  docker exec mounts for agents... ##########"
 echo "##############################################################"
+echo ""
 
 while [ $counta -lt ${AGENTS} ]
 do
     echo "working on docker exec for agent:" $counta
-    docker exec k3d-k3s-agent-$counta sh -c "mount --make-shared /run/cilium/cgroupv2"
+    while [[ $(docker exec k3d-k3s-agent-"${counta}" sh -c "mount --make-shared /run/cilium/cgroupv2") -eq 1 ]]
+    do
+        echo "Houston, we have a problem. Result is no good:"$?
+        echo "trying mount again"
+        sleep 5
+    done
+    
     ((counta++))
-    sleep
+    sleep 3
 done
+
+echo ""
+echo "##############################################################"
+echo "######## labeling agents as workers to look good... ##########"
+echo "##############################################################"
+echo ""
 
 counta=0
 while [ $counta -lt ${AGENTS} ]
@@ -126,6 +216,17 @@ do
   ((counta++))
 done
 
+echo ""
+echo "##############################################################"
+echo "###### kubectl get nodes to check current status... ##########"
+echo "##############################################################"
+echo ""
+
 kubectl get nodes
 sleep 5
+echo ""
+echo "##############################################################"
+echo "###### kubectl getting pods to check where we are... #########"
+echo "##############################################################"
+echo ""
 kubectl get po -A
